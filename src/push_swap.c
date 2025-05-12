d7e288d29792ce6b4fc2db71c0aa695d094d87e3
fyudris <fanny.yudris@gmail.com>
2025-05-12 17:52:48 +0200

    Linking libft and printf. Create push_swap.h.

---
+++ src/push_swap.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fyudris <fyudris@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/19 12:40:59 by fyudris           #+#    #+#             */
+/*   Updated: 2025/05/12 17:43:01 by fyudris          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include//push_swap.h"
+
+int	main(int argv, char **argc)
+{
+	t_stack_node	*a;
+	t_stack_node	*b;
+
+	a = NULL;
+	b = NULL;
+
+	// Check argument count
+	if (argc == 1 ||)
+}
