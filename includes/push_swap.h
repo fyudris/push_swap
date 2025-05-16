ecaefc3d665d8374ccdf88900efad113039d0007
fyudris <fanny.yudris@gmail.com>
2025-05-16 22:48:55 +0200

    Adding ft_free_array and ft_atol to libft. Add parse.c

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: fyudris <fyudris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 12:30:05 by fyudris           #+#    #+#             */
-/*   Updated: 2025/05/16 18:59:17 by fyudris          ###   ########.fr       */
+/*   Updated: 2025/05/16 22:09:53 by fyudris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,15 @@ typedef struct s_stack_node
 	struct s_stack_node		*next;
 	struct s_stack_node		*prev;
 }	t_stack_node;
+
+// Parsing
+bool	is_valid_input(int argc, char **argv);
+bool	is_valid_input(int argc, char **argv);
+char	**join_and_split_args(int argc, char **argv, int *count);
+bool	is_number(char *str);
+long	ft_atol(const char *str);
+void	free_array(char **array);
+
 
 // Error Handling
 
